olsonse/kafs-client

commit f058d9bb7b22a431b17a56242ab7e07e5187b1cd

Use syslog in kafs-preload program
Write verbose messages to the syslog in the kafs-preload programme and also
set the name of the facility.

Signed-off-by: David Howells <dhowells@redhat.com>

src/preload-cells.c
@@ -35,6 +35,12 @@ static void verbose(const char *fmt, ...)
 	va_list va;
 
 	va_start(va, fmt);
+	if (isatty(2)) {
+		vprintf(fmt, va);
+		putchar('\n');
+	} else {
+		vsyslog(LOG_INFO, fmt, va);
+	}
 	vprintf(fmt, va);
 	putchar('\n');
 	va_end(va);
@@ -137,6 +143,11 @@ int main(int argc, char *argv[])
 			usage(argv[0]);
 			break;
 		}
+	}
+
+	if (!redirect_to_stdout) {
+		openlog("kafs-preload", 0, LOG_USER);
+		syslog(LOG_NOTICE, "kAFS: Preloading cell database");
 	}
 
 	argc -= optind;

commit caa05b9593333b3e6198797c61d43bb1d326a3d9

Fix verbose() func in kafs-preload
When kafs-preload's verbose() function was made to support syslog, the
original vprintf was left in and thus duplicated.  However, the va pointer
has been advanced and so this SEGV's.

Fixes: f058d9bb7b22 ("Use syslog in kafs-preload program")
Signed-off-by: David Howells <dhowells@redhat.com>

src/preload-cells.c
@@ -41,8 +41,6 @@ static void verbose(const char *fmt, ...)
 	} else {
 		vsyslog(LOG_INFO, fmt, va);
 	}
-	vprintf(fmt, va);
-	putchar('\n');
 	va_end(va);
 }
 